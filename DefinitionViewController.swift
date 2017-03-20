4f7d992dd2d090900011d1c663441480a5236a65
Michael Zielinski <mjziel@msn.com>
2017-03-20 17:27:59 -0400

    finished it

---
+++ DefinitionViewController.swift
@@ -0,0 +1,84 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Michael Zielinski on 3/20/17.
+//  Copyright © 2017 Worldengine. All rights reserved.
+//
+
+//* Added file for new view controller class. File -> New -> Cocoa Touch Class
+//* Go back to main storyboard. Connect new view controller to second storyboard.
+//* Select view controller, newspaper, add name of new View Controller as Class
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    //* connect emoji label to var emojilabel
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    //* title for the emoji
+    @IBOutlet weak var emojiTitle: UILabel!
+    //* definition for the emoji
+    @IBOutlet weak var emojiDefine: UILabel!
+    
+    //* this variable gets changed in segue
+    var emoji = "No Emoji"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //* use the emoji variable to set emojilabel
+        emojiLabel.text = emoji
+        
+        //* use switch statement to assign title and definition
+        
+        switch emoji {
+        case "😀":
+            emojiTitle.text = "Smiley"
+            emojiDefine.text = "Use to express happiness."
+            break
+        case "😂":
+            emojiTitle.text = "Crying Laughing"
+            emojiDefine.text = "Use when something is so funny you are crying and laughing."
+            break
+        case "😅":
+            emojiTitle.text = "Sweating"
+            emojiDefine.text = "Use when you were nervous about something and sweating, but now are relieved."
+            break
+        case "🤐":
+            emojiTitle.text = "Not Talking"
+            emojiDefine.text = "Use when you are not allowed to talk about something."
+            break
+        case "😎":
+            emojiTitle.text = "Cool Sunglasses"
+            emojiDefine.text = "Use when there is too much sunlight."
+            break
+        case "👽":
+            emojiTitle.text = "Alien"
+            emojiDefine.text = "Use when you see an alien or are an alien."
+            break
+        case "🤖":
+            emojiTitle.text = "Robot"
+            emojiDefine.text = "Use when you see a robot or are a robot."
+            break
+        case "👿":
+            emojiTitle.text = "Evil Purple"
+            emojiDefine.text = "Use when someone is evil and/or purple."
+            break
+        default:
+            emojiTitle.text = ""
+            emojiDefine.text = ""
+        }
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,12 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+//* added UITableViewDataSource, UITableViewDelegate
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //* table view outlet
+    @IBOutlet weak var tableView: UITableView!
+    
+    //* an array of emojis
+    var emojis = ["😀","😂","😅","😎","🤐","👽","🤖","👿"]
+    var emojiDesc =
+        ["Smiley", "Crying Laughing", "Sweating", "Cool Sunglasses", "Not Talking", "Alien", "Robot", "Evil Purple"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //* next two lines were added
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+    
+    //* specify the number of rows in section for table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //* return the length of the array
+        return emojis.count
+    }
+    
+    //* specify cell for row at index path for table view
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //* set up UITableViewCell to return
+        let cell = UITableViewCell()
+        //* assign emoji from array to cell textLabel using indexPath.row
+        cell.textLabel?.text = emojis[indexPath.row] + "   " + emojiDesc[indexPath.row]
+        //* return the cell
+        return cell
+    }
+    
+    //* from navigation created segue moveSegue, added this function with didSelectRowAt
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //* added performSegue assign emoji as sender
+        performSegue(withIdentifier: "moveSegue", sender: emojis[indexPath.row])
+    }
+    
+    //* Editor -> Embed In -> Navigation Controller (Back Button)
+    
+    //* add prepare for segue to get next view controller ready
+    //* Added DefinitionViewController Class
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //* create constant holding new view controller
+        let defVC = segue.destination as! DefinitionViewController
+        //* assign sender to emoji var in new view controller
+        defVC.emoji = sender as! String
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
